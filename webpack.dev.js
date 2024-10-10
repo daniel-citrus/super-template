f87ca8830e17694def670f4f4955bf744c43779a
Daniel <dandcalvo@gmail.com>
2024-10-10 15:14:40 -0700

    Complete setup

---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+});
