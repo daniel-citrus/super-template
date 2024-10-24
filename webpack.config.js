8bcffef4ad0b960378c85b02155a0c35ff8e59d3
Daniel <dandcalvo@gmail.com>
2024-10-24 15:54:25 -0700

    setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = (end, argv) => {
+    const devMode = argv.mode !== 'production';
+
+    return {
+        entry: './src/index.js',
+        module: {
+            rules: [
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+        output: {
+            clean: true,
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Project',
+                template: './src/template.html',
+            }),
+        ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    };
+};
